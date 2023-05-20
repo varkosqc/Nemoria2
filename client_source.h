cf34b187935c60ecc3fac9974e5fbb555845f0e4
varkosqc <100730901+varkosqc@users.noreply.github.com>
2023-05-20 10:05:25 -0400

    Update client_source.h

--- client_source.h
+++ client_source.h
@@ -9,48 +9,48 @@
 #define LOCALE_SERVICE_EUROPE
 
 /*** YMIR System ***/
-#define ENABLE_ACCE_COSTUME_SYSTEM			   	// Sash costume system
-#define ENABLE_COSTUME_SYSTEM					// Costume system
-#define ENABLE_DRAGON_SOUL_SYSTEM				// Dragon soul system
-#define ENABLE_DICE_SYSTEM				 	// Dice system
-#define ENABLE_ENERGY_SYSTEM					// Energy system
-#define ENABLE_GLOVE_SYSTEM					// Glove system
-#define ENABLE_MOUNT_COSTUME_SYSTEM				 // Mount system
-#define ENABLE_MOVE_CHANNEL					// Channel changer system
-#define ENABLE_NEW_EQUIPMENT_SYSTEM				 // New equipement system
-#define ENABLE_PENDANT_SYSTEM					// Pendant system
-#define ENABLE_QUIVER_SYSTEM					// Quiver system
-#define ENABLE_WEAPON_COSTUME_SYSTEM				// Weapon costum system
+#define ENABLE_ACCE_COSTUME_SYSTEM
+#define ENABLE_COSTUME_SYSTEM
+#define ENABLE_DRAGON_SOUL_SYSTEM
+#define ENABLE_DICE_SYSTEM
+#define ENABLE_ENERGY_SYSTEM
+#define ENABLE_GLOVE_SYSTEM
+#define ENABLE_MOUNT_COSTUME_SYSTEM	
+#define ENABLE_MOVE_CHANNEL
+#define ENABLE_NEW_EQUIPMENT_SYSTEM
+#define ENABLE_PENDANT_SYSTEM
+#define ENABLE_QUIVER_SYSTEM
+#define ENABLE_WEAPON_COSTUME_SYSTEM
 
 /*** N2 - Features ***/
 #define ENABLE_4TH_AFF_SKILL_DESC
 #define ENABLE_CANSEEHIDDENTHING_FOR_GM
-#define ENABLE_DISCORD_RPC					// Show discord activity
-#define ENABLE_EMOJI_SYSTEM					// Emoji system (chat)
-#define ENABLE_EXTEND_INVEN_SYSTEM				 // 4 inventory pages
-#define ENABLE_HIGHLIGHT_NEW_ITEM				// Hightlight new item
+#define ENABLE_DISCORD_RPC
+#define ENABLE_EMOJI_SYSTEM
+#define ENABLE_EXTEND_INVEN_SYSTEM
+#define ENABLE_HIGHLIGHT_NEW_ITEM
 #define ENABLE_LEVEL_IN_TRADE
 #define ENABLE_LVL115_ARMOR_EFFECT
-#define ENABLE_MINIMAP_WHITEMARK_CIRCLE				// Circle whitemark on minimap
+#define ENABLE_MINIMAP_WHITEMARK_CIRCLE
 #define ENABLE_MOUSEWHEEL_EVENT
 #define ENABLE_NO_DSS_QUALIFICATION
 #define ENABLE_OBJ_SCALLING
 #define ENABLE_PACK_GET_CHECK
 #define ENABLE_PET_SYSTEM_EX
-#define ENABLE_PLAYER_PER_ACCOUNT5				// 5 characters per account
-#define ENABLE_PROTOSTRUCT_AUTODETECT				// Auto dectect proto structure
+#define ENABLE_PLAYER_PER_ACCOUNT5
+#define ENABLE_PROTOSTRUCT_AUTODETECT
 #define ENABLE_SLOT_WINDOW_EX
-#define ENABLE_SLOT_COVER_IMAGE_SYSTEM			// Slot cover image system
-#define ENABLE_STEALTH_FIX						// Fix stealth (assassin)
-#define ENABLE_TEXT_LEVEL_REFRESH				// Level refresh (sync)
-#define ENABLE_USE_COSTUME_ATTR					// Bonus on costume
-#define ENABLE_HIDE_KINGDOM_FLAGS				// Hide kingdom flags
-#define ENABLE_SWAP_SLOT_SYSTEM					// Quick swap slot system
-#define ENABLE_NEW_DROP_DIALOG					// New drop dialog (destroy item)
+#define ENABLE_SLOT_COVER_IMAGE_SYSTEM
+#define ENABLE_STEALTH_FIX
+#define ENABLE_TEXT_LEVEL_REFRESH
+#define ENABLE_USE_COSTUME_ATTR
+#define ENABLE_HIDE_KINGDOM_FLAGS
+#define ENABLE_SWAP_SLOT_SYSTEM
+#define ENABLE_NEW_DROP_DIALOG
 
 /*** N2 - System ***/
-#define ENABLE_PLAYER_PIN_SYSTEM				// Player secret code
-#define ENABLE_HIDE_COSTUME_SYSTEM				// Hide system (costume, hair, sash, weapon)
+#define ENABLE_PLAYER_PIN_SYSTEM
+#define ENABLE_HIDE_COSTUME_SYSTEM
 
 /*** WJ Features ***/
 #define WJ_SHOW_MOB_INFO
